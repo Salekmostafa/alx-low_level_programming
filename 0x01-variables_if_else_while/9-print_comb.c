41c75c3981233d6938d4f5d8cf9236c6b7fe0396
Salekmostafa <salekelmosta@gmail.com>
2023-08-14 02:17:44 +0200

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
+
