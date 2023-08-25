2a21745bf46a23bf9054e6db630f62f8f44933a0
Salekmostafa <salekelmostafa@gmail.com.com>
2023-08-25 16:37:46 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int length_of_dest, z;
+	int length_of_dest, z;
 
-    length_of_dest = 0;
-    while (dest[length_of_dest] != '\0')
-        length_of_dest++;
+	length_of_dest = 0;
+	while (dest[length_of_dest] != '\0')	
+	length_of_dest++;
 
-    for (z = 0; src[z] != '\0'; z++, length_of_dest++) {
-        dest[length_of_dest] = src[z];
-    }
-    dest[length_of_dest] = '\0';
+	for (z = 0; src[z] != '\0'; z++, length_of_dest++)
+	{
+		dest[length_of_dest] = src[z];
+	}
+	dest[length_of_dest] = '\0';
 
-    return dest;
+	return dest;
 }
