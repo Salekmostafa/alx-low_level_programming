d37260c64282c1a52fc4115c9a0af80a8dc0e7dd
Salekmostafa <salekelmostafa@gmail.com>
2023-08-29 01:32:55 +0200

    aaaaa

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,31 +1,17 @@
-#include "main.h"
-#include <stdio.h>
+nclude "main.h"
 
-void print_board(char (*a)[8])
+/**
+ * print_chessboard - Entry pt
+ * @: array
+ * Return: 0
+*/
+void print_chessboard(char (*a)[8])
 {
-    for (int i = 0; i < 8; i++)
+    int i, j;
+    for (i = 0; i < 8; i++)
     {
-        for (int j = 0; j < 8; j++)
-        {
-            printf("%c ", a[i][j]);
-        }
-        printf("\n");
+        for (j = 0; j < 8; j++)
+            _putchar(a[i][j]);
+        _putchar("\n");
     }
-}
-
-int main() {
-    char board[8][8] = {
-        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
-    };
-
-    print_board(board);
-
-    return 0;
 }
