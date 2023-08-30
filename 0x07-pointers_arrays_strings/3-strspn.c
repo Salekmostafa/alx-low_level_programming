c660bae7bd60a858e67e39def07a33d13681c7b5
Salekmostafa <salekelmostafa@gmail.com>
2023-08-30 02:46:58 +0200

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,35 @@
 #include "main.h"
 
 /**
- * _strspn - Entry pt
+ * _strspn - entry pt
  * @s: input
  * @accept: input
- * Return: Always 0
-*/
+ * Return: 0
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+    unsigned int i, n, value;
+    value = 0;
 
-	value = 0;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        int check = 0;
+        
+        for (n = 0; accept[n] != '\0'; n++)
+        {
+            if (accept[n] == s[i])
+            {
+                value++;
+                check = 1;
+                break;
+            }
+        }
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		check = 0;
-
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
-		}
-	}
-
+        if (check == 0) {
+            break;
+        }
+    }
+    
+    return value;
 }
