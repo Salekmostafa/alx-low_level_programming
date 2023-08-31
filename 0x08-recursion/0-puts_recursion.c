a1ab6489107ff6e93022e904f2260e990eb57426
Salekmostafa <salekelmostafa@gmail.com.com>
2023-08-31 16:44:22 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print
+ * @s: pointer
+ * Return: void
+*/
+
+void _puts_recursion(char *s) {
+    if (*s == '\0') {
+        putchar('\n');
+        return;
+    }
+    else
+    {
+        putchar(*s);
+        _puts_recursion(s + 1);
+    }
+}
