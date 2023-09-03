54de4e7b080d9bcc845e7dd3cc88fa2e813de206
Salekmostafa <salekelmostafa@gmail.com.com>
2023-09-03 07:30:40 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,22 +9,21 @@
 int square(int n, int x);
 int _sqrt_recursion(int n)
 {
-    return (square(n, 1));
+	return (square(n, 1));
 }
 
 /**
- * square - find square root
- * @n: int
- * @x: square root
- * Return: int
+* square - find square root
+* @n: int
+* @x: square root
+* Return: int
 */
 int square(int n, int x)
 {
-    if (x * x == n)
-        return (x);
-    else if (x * x < n)
-        return (square(n, x + 1));
-    else
-        return (-1);
-
+	if (x * x == n)
+	return (x);
+	else if (x * x < n)
+	return (square(n, x + 1));
+	else
+	return (-1);
 }
