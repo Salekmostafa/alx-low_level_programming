ab0aa21315c60a9ef2a2dff6ded79e90166cba8d
Salekmostafa <salekelmostafa@gmail.com.com>
2023-09-03 14:57:41 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function to print
- * @s: pointer 
- * Return: void
+* _print_rev_recursion - prints string
+* @s: ptr to string
+* Return: void
 */
 
-void _puts_recursion(char *s) {
-if (*s == '\0') {
-putchar('\n');
-return;
-}
-else
+void _print_rev_recursion(char *s)
 {
-_puts_recursion(s + 1);
-putchar(*s);
+if (*s > '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
