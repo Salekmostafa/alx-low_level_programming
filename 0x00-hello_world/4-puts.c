91b0cd97070e53fcf0934658a0a063ed88270492
Salekmostafa <salekelmosta@gmail.com>
2023-11-23 22:55:24 +0100

    bit_manupulation

--- 0x00-hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * main - Program with puts function
- * Return: 0 (Success)
- */
-int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
--- 0x08-recursion/README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-readme
-=======
-Readme
->>>>>>> a35b6493991dd6b8734710f8ec9ed24a667ddd5c
